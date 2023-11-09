132b97495a8c05ef0ea0385b4a0b4542569d615d
Jonas Klein <jonasklein2001@gmail.com>
2023-11-09 14:29:02 +0100

    Updates for docker

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request, redirect, session, u
 from datetime import datetime, timedelta
 import secrets
 from database import load_courses_from_db, load_random_courses_from_db, load_explore_courses_from_db, load_last_viewed_courses_from_db, load_favorite_courses_from_db, add_click_to_db, search_courses_from_db
-from open_ai import print_recommendations_from_strings
+from ai_rec import print_recommendations_from_strings
 from content_based import get_content_based_courses
 
 app = Flask(__name__)
