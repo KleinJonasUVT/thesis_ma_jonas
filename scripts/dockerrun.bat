717d8c641359007e18df3cafb0057731a21c317e
RobertSmoldersTUe <54588370+RobertSmolders@users.noreply.github.com>
2024-07-27 22:35:50 +0200

    remove bat scripts

--- scripts/dockerrun.bat
+++
@@ -1,6 +0,0 @@
-SET IMAGE=tai-coursecatalogue
-SET VERSION=0.0.1
-SET CERT_FILE_PATH=/app/cert.pem
-
-CALL docker build -t %IMAGE%:%VERSION% .
-CALL CALL docker run -d -p 80:80 --env-file secrets\prod.env %IMAGE%:%VERSION%
